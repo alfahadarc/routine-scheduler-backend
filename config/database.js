668f5d32e7b193945bff5e7dcd499e29f7bbf215
alfahadarc <alfahadarc@gmail.com>
2023-07-30 00:38:13 +0600

    db connected

---
+++ config/database.js
@@ -0,0 +1,21 @@
+import dotenv from 'dotenv'
+dotenv.config()
+import pkg from 'pg';
+const { Pool } = pkg;
+
+console.log(process.env.CONNECTION_URL);
+const pool = new Pool({
+    connectionString: process.env.CONNECTION_URL,
+    ssl: {
+        rejectUnauthorized: false
+    },
+    max: 5,
+    idleTimeoutMillis: 30000,
+    connectionTimeoutMillis: 5000,
+})
+
+const connect = async () => {
+    return await pool.connect()
+}
+
+export { connect }
--- index.js
+++ index.js
@@ -1,6 +1,23 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { connect } from './config/database.js'
+
 const app = express()
 const port = 3000
+
+
+connect().then(
+  (client) => {
+    console.log(client);
+  }
+).catch(
+  (error) => {
+    console.log(error)
+  }
+)
+
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
