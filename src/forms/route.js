7d315b6dda2d83800928ef53110c8a579e6573aa
alfahadarc <alfahadarc@gmail.com>
2023-08-06 19:38:36 +0600

    edited some files

--- src/forms/route.js
+++ src/forms/route.js
@@ -1,10 +1,22 @@
+
 import express from 'express.js'
+
 const router = express.Router();
 
+// form - > key[teacher_initial_preferences/schedule], type[theory/sessional], response
 
+// theory/pref/{form_id} get
 
+import { sendTheoryMail } from './controller.js';
 
+formRouter.get("/theory/pref",sendTheoryMail)
 
+// theory/pref/{form_id} postMessage
+// theory/schedule/{form_id} get
+
+// theory/schedule/{form_id} post
+// sessional/pref/{form_id} get
+// sessional/pref/{form_id} post
 
 
 export default router;
