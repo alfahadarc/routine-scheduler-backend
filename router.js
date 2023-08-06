23d7aa6ec15fb2b0a531ab51add4d83265c94cd4
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2023-08-07 01:53:28 +0600

    Added Auth Middleware

--- router.js
+++ router.js
@@ -3,10 +3,9 @@ import authRouter from "./src/auth/router.js";
 import teacherRouter from "./information/teacher/route.js";
 import sectionRouter from "./information/section/route.js";
 import roomRouter from "./information/room/route.js";
-import courseRouter from "./information/courses/route.js"
-import formsRouter from "./src/forms/route.js"
-import assignRouter from "./src/assignment/route.js"
-
+import courseRouter from "./information/courses/route.js";
+import formsRouter from "./src/forms/route.js";
+import assignRouter from "./src/assignment/route.js";
 
 import { authorize } from "./config/authorize.js";
 
@@ -14,10 +13,11 @@ const router = express.Router();
 
 router.use("/auth", authRouter);
 router.use("/teacher", authorize(), teacherRouter);
-router.use("/section", sectionRouter);
-router.use("/room", roomRouter);
-router.use("/forms",formsRouter);
-router.use("/assign",assignRouter);
-router.use("/course",courseRouter);
+router.use("/section", authorize(), sectionRouter);
+router.use("/room", authorize(), roomRouter);
+router.use("/assign", authorize(), assignRouter);
+router.use("/course", authorize(), courseRouter);
+
+router.use("/forms", formsRouter);
 
 export default router;
