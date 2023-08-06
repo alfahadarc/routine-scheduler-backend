3d3649199b174125f3e6e1734047ff8693bff598
alfahadarc <alfahadarc@gmail.com>
2023-08-07 03:10:48 +0600

    form get by uuid

--- src/assignment/repository.js
+++ src/assignment/repository.js
@@ -32,12 +32,12 @@ export async function getAllTeacherMail() {
 
 }
 
-export async function createForm(id, initial,type){
+export async function createForm(id, initial, type) {
     const query = 'INSERT INTO forms (id, type, initial) VALUES ($1, $2, $3)'
     const values = [id, type, initial]
 
     const client = await connect()
-    const results = await client.query(query,values)
+    const results = await client.query(query, values)
 
     if (results.rowAffected <= 0) {
         throw new Error("Insertion Failed");
@@ -48,11 +48,25 @@ export async function createForm(id, initial,type){
 
 }
 
-export async function getTheoryPreferencesStatus(){
+export async function getTheoryPreferencesStatus() {
     const query = `
     SELECT response, teachers.initial, teachers.name, teachers.email
     FROM forms
     INNER JOIN teachers ON forms.initial = teachers.initial
+    `;
+
+    const client = await connect()
+    const results = await client.query(query)
+    client.release()
+
+    return results.rows
+}
+
+export async function finalize() {
+    const query = `
+    SELECT value
+    FROM public.configs
+    WHERE "key" = 'THEORY_PREFERENCES_COMPLETE';
     `;
 
     const client = await connect()
--- src/forms/repository.js
+++ src/forms/repository.js
@@ -1,20 +1,58 @@
 import { connect } from '../../config/database.js'
 
+async function getCourses(type){
+    //theory-pref, theory-sched,sessional-pref
 
+    const query = "SELECT value FROM configs WHERE key='CURRENT_SESSION'"
+    const client = await connect()
+    const results = await client.query(query)
+    const current_session = results.rows[0].value
+    client.release()
+
+    if(type=="theory-pref"){
+        const query = `SELECT course_id, name
+        FROM courses
+        where type =0 and session = $1
+        `;
+        const values =[current_session]
+        const client = await connect()
+        const results = await client.query(query,values)
+        client.release()
+        return results.rows
+
+    }else if(type=="theory-sched"){
+
+    }else if(type=="sessional-pref"){
+
+    }
+}
 
 export async function getFormByUUID(uuid) {
 
-    const query = 'SELECT * FROM forms WHERE id=$1'
+    const query = `
+    SELECT type, teachers.initial, teachers.name
+    FROM forms
+    INNER JOIN teachers ON forms.initial = teachers.initial
+    WHERE id=$1
+    `;
+ 
     const values = [uuid]
 
     const client = await connect()
     const results = await client.query(query, values)
 
+    const courses =await getCourses(results.rows[0].type)
+    console.log(results.rows)
+    const data ={
+        teachers: results.rows[0],
+        courses:courses
+    }
+
     if (results.rows.length <= 0) {
         throw new Error("Table is empty");
     } else {
         client.release();
-        return results.rows;
+        return data;
     }
 
 }
