3b9c0c156b0b430991956097254855de6473cca8
alfahadarc <alfahadarc@gmail.com>
2023-08-06 16:30:41 +0600

    cors added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import cors from 'cors'
 import dotenv from 'dotenv'
 dotenv.config()
 
@@ -8,6 +9,8 @@ import errorHandler from './config/error-handle.js'
 
 const app = express()
 const port = 8000
+
+app.use(cors())
 
 app.use(express.json());
 
