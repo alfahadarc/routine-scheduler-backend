d4229158109f6821db64b240929fe725367c4ac6
Md. Jehadul Karim <sabit.jehadul.karim@gmail.com>
2023-08-19 16:48:53 +0600

    Token Header

--- config/authorize.js
+++ config/authorize.js
@@ -6,18 +6,18 @@ const secret = process.env.SECRET;
 
 export function verifyToken(req, res, next) {
     const token =
-        req.body.token || req.query.token || req.headers["x-access-token"];
+        req.body.token || req.query.token || req.headers["x-access-token"] || req.headers['authorization'] && req.headers['authorization'].split(' ')[1];
 
     if (!token) {
         return res
             .status(401)
-            .json(message.error("A token is required for authentication"));
+            .json({message: "A token is required for authentication"});
     }
     try {
         const decoded = jwt.verify(token, secret);
         req.user = decoded;
     } catch (err) {
-        return res.status(401).json(message.error("Invalid token"));
+        return res.status(401).json({message: "Invalid token"});
     }
     return next();
 };
@@ -36,7 +36,7 @@ export function authorize()  {
             next();
 
         } catch (err) {
-            return res.status(401).json(message.error("Invalid Token"));
+            return res.status(401).json({message: "Invalid Token"});
         }
     };
 };
