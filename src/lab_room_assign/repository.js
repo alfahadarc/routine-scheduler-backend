9a0f3f1fadbe93233b30f39f422d0958a5826dce
SomonindroRoy <1805049@ugrad.cse.buet.ac.bd>
2023-08-20 19:50:08 +0600

    lab room assign done w/o post

--- src/lab_room_assign/repository.js
+++ src/lab_room_assign/repository.js
@@ -18,9 +18,10 @@ export const getAllRoom = async () => {
 
 export const getAllCourse = async () => {
 
-    const query = 'SELECT cs.course_id, cs.section, cs.batch , c.name\
+    const query = 'SELECT cs.course_id, cs.section, cs.batch , c.name, s.level_term \
                         FROM courses_sections cs\
                         JOIN courses c ON cs.course_id = c.course_id\
+                        join sections s using (batch, section)\
                         WHERE cs.course_id LIKE \'CSE%\' and c.type=1';
     const client = await connect()
     const results = await client.query(query)
